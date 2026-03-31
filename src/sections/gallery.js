870dbba107621d3d5833b3ff3d52a46a4abe4e3b
Eric Huang <ejwhuang@gmail.com>
2026-03-30 20:58:59 -0400

    setup new sections

---
+++ src/sections/gallery.js
@@ -0,0 +1,9 @@
+export function render() {
+  return `
+    <section id="gallery" class="section-full flex items-center justify-center font-akshar bg-gray-100">
+      <h1 class="text-5xl sm:text-7xl lg:text-8xl xl:text-[110px] font-medium text-black/20">GALLERY</h1>
+    </section>
+  `;
+}
+
+export function init() {}
---
+++ src/sections/recap.js
@@ -0,0 +1,9 @@
+export function render() {
+  return `
+    <section id="recap" class="section-full flex items-center justify-center font-akshar bg-camd-gray">
+      <h1 class="text-5xl sm:text-7xl lg:text-8xl xl:text-[110px] font-medium text-black/20">RECAP</h1>
+    </section>
+  `;
+}
+
+export function init() {}
