3a4224c894e2311a291ebef37b9bf6076ef37cb3
Ethan Cabalona <cabalona.e@northeastern.edu>
2026-04-06 02:06:33 -0400

    initialized sanity client, created gallery with modal for desktop. Note: Sanity server CORS not enabled for production environment yet

---
+++ src/sanity/imageBuilder.js
@@ -0,0 +1,7 @@
+import { createImageUrlBuilder } from "@sanity/image-url";
+import { client } from "../sanity/client";
+
+const builder = createImageUrlBuilder(client);
+export function urlFor(source) {
+  return builder.image(source);
+}
